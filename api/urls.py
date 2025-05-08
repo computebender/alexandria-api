93a1fe64ba4b4accbc6a6386130c52e3fd3f847b
Ben Munro <ben.munro2000@gmail.com>
2025-05-08 19:26:40 -0400

    add journal entries

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import HelloWorldView
+from django.urls import include, path
+from .views import HelloWorldViewSet, JournalEntryViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'journal-entries', JournalEntryViewSet)
+router.register(r'hello-world', HelloWorldViewSet, basename='hello-world')
 
 urlpatterns = [
-    path('hello/', HelloWorldView.as_view(), name='hello_world'),
+    path('', include(router.urls)),
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
-class HelloWorldView(APIView):
-    def get(self, request):
+
+from .serealizers import JournalEntrySerializer
+from .models import JournalEntry
+
+class HelloWorldViewSet(viewsets.ViewSet):
+    def list(self, request):
         return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
+    
+class JournalEntryViewSet(viewsets.ModelViewSet):
+    queryset = JournalEntry.objects.all()
+    serializer_class = JournalEntrySerializer
